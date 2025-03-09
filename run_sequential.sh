1f0c8e6614a77cac96d634ed079af3ba3750e5ec
gpwhs <george.searle34@gmail.com>
2025-03-09 22:20:49 +0000

    fix configs

--- run_sequential.sh
+++ run_sequential.sh
@@ -10,12 +10,14 @@ if [ $? -eq 0 ]; then
 else
     echo "First script failed. Second script will not run."
     exit 1
+fi
 if [ $? -eq 0 ]; then
     echo "Second script completed successfully. Running third script..."
     python biobank_classification.py --config xgb_config_t2d.yaml
 else
     echo "Second script failed. Third script will not run."
     exit 1
+fi
 if [ $? -eq 0 ]; then
     echo "Third script completed successfully. Running fourth script..."
     python biobank_classification.py --config lr_config_t2d.yaml
